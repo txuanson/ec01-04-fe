e437f1228db349ab00f867053cf677c5fd868155
tktruc211 <tktruc19@clc.fitus.edu.vn>
2022-08-24 22:35:21 +0700

    Introduce interface

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header2 from "../components/layout/Header2";
+import Introduce from "../components/layout/Introduce";
+import Banner from "../components/layout/Banner";
+
+export default function AboutUs() {
+  return (
+    <>
+      <Header2 />
+      <Introduce />
+      <Banner />
+    </>
+  );
+}
